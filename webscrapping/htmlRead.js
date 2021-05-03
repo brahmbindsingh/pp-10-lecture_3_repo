3f8b3b4f8bf22af8a5e16939c09a22fecb1b6721
brahmbindsingh <singhbrahmbind@gmail.com>
2021-05-03 14:33:37 +0530

    checking

--- webscrapping/htmlRead.js
+++ webscrapping/htmlRead.js
@@ -1,13 +1,23 @@
 const fs = require("fs");
 const cheerio = require("cheerio");
 
-let htmlKaData = fs.readFileSync("./index.html");
+let htmlKaData = fs.readFileSync("./index.html").toString();
+//console.log(htmlKaData);
 
 let ch = cheerio.load(htmlKaData);
 
 // <h1>Heading 1</h1> => object form !!
-let liKaObject = ch("li").text();
-console.log(liKaObject);
+//let liKaObject = ch("p");
 
-// let h1KaData = ch("h1").`text();
-// console.log(h1KaData);`
+//console.log(liKaObject);
+
+let h1KaData = ch("h1").text();
+//console.log(h1KaData);
+
+let pTags = ch("p");
+
+//ch dala kyoki .text sirf outer obj ko padhta hai isse vo inner obj ko bhi padega
+//console.log(ch(pTags["2"]).text()); 
+
+let pLiKeAndar = ch("li>p").text();
+console.log(pLiKeAndar);
