756a6f4a140f5c69b4953b5f7deb5e88f89ddec7
kirat <singhkiratmehar@gmail.com>
2021-05-20 23:12:17 +0530

    late

--- new_dev/activity.js
+++ new_dev/activity.js
@@ -1,6 +1,8 @@
 let cheerio = require("cheerio");
 let fs = require("fs");
 let request = require("request");
+let jspsf = require("jspdf");
+let topicLinks;
 
 request("https://github.com/topics",callback);
 
@@ -9,10 +11,35 @@ function callback(error,response,html){
         let manipulationTool = cheerio.load(html);
         let allAnchorTags = manipulationTool(".no-underline.d-flex.flex-column.flex-justify-center");
         for(let i=0;i<allAnchorTags.length;i++){
-            let linkName = manipulationTool(allAnchorTags[i]).find("p.f3");
+            let linkName = manipulationTool(manipulationTool(allAnchorTags[i]).find("p.f3")).text().trim();
             let singleLink = "https://github.com" + manipulationTool(allAnchorTags[i]).attr("href");
+            topicProcesser(singleLink,linkName);
+
+            // folderCreater("D:/pepwed/new_dev/pro_issue/"+linkName.text().trim());
             
-            console.log(linkName.text(),singleLink.text());
         }
     }
 }
+
+// function folderCreater(name){
+//     if(!fs.existsSync(name)){
+//         fs.mkdirSync(name);
+//     }
+// }
+
+function topicProcesser(url,topicName){
+    request(url,function(error,response,html){
+        if(!error){
+            const manipulationTool = cheerio.load(html);
+            let projectAtt = manipulationTool(".f3.color-text-secondary.text-normal.lh-condensed .text-bold");
+            projectAtt =  projectAtt.slice(0,4);
+            for(let i=0;i<projectAtt.length;i++){
+                console.log(manipulationTool(projectAtt[i]).text());
+                console.log(manipulationTool(projectAtt[i]).attr("href"));
+            }
+        }
+    });
+}
+
+
+topicProcesser("https://github.com/topics/latex");
