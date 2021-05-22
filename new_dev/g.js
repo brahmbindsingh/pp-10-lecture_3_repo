ae8408235c3225816d09b29ca44c738fd1b0e93c
brahmbindsingh <singhbrahmbind@gmail.com>
2021-05-22 10:00:42 +0530

    last work

--- new_dev/g.js
+++ new_dev/g.js
@@ -1,6 +1,7 @@
 const cheerio = require("cheerio");
 const fs = require("fs");
 const request = require("request");
+let data = {};
 
 request("https://github.com/topics",callback);
 
@@ -20,17 +21,47 @@ function callback(error,response,html){
 
 function projectSelector(name,url){
     request(url,function(error,response,html){
-        const mt = cheerio.load(html);
-        //project selector
-        let projectDesc = mt(".f3.color-text-secondary.text-normal.lh-condensed .text-bold").slice(0,5);
-        console.log(name);
-        console.log("------------------------------------------------");
-        for(let x=0;x<projectDesc.length;x++){
-            let projectName = mt(projectDesc[x]).text().trim();
-            let projectLink = mt(projectDesc[x]).attr("href");
-            console.log(projectName);
-            console.log(projectLink);
+        if(!error){
+            const mt = cheerio.load(html);
+            //project selector
+            let projectDesc = mt(".f3.color-text-secondary.text-normal.lh-condensed .text-bold").slice(0,5);
+            // console.log(name);
+            console.log("------------------------------------------------");
+            for(let x=0;x<projectDesc.length;x++){
+                let projectName = mt(projectDesc[x]).text().trim();
+                let projectLink = "https://github.com"+mt(projectDesc[x]).attr("href");
+                if(!data[name]){
+                    data[name] = [];
+                    data[name].push({
+                        name: projectName,
+                    })
+                }
+                else{
+                    data[name].push({
+                        name: projectName,
+                    })
+                }
+                projectProcessor(projectLink,name,projectName)
+                // console.log(projectName);
+                // console.log(projectLink);
+            }
+            // console.log("------------------------------------------------");
+            fs.writeFileSync("data1.json",JSON.stringify(data));
         }
-        console.log("------------------------------------------------");
+    });
+}
+
+function projectProcessor(projectUrl,topicName,ProjectName){
+    projectUrl = projectUrl+"/issues";
+    response(projectUrl,function(error,response,html){
+        const mt = cheerio.load(html);
+
+        let index = -1;
+
+        let issueAnchors = mt(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title").slice(0,5);
+        for(let i=0;i<issueAnchors.length;i++){
+            let link = "https://github.com"+mt(issueAnchors[i]).attr("href").trim();
+            let name = mt(issueAnchors[i]).text().trim();
+        }
     });
 }
