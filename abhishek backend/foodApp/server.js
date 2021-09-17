cf875e36519da704b5607d259b15958db9587e54
Brahm Bind Singh <singhbrahmbind@gmail.com>
2021-09-17 14:50:56 +0530

    day1

--- abhishek backend/foodApp/server.js
+++ abhishek backend/foodApp/server.js
@@ -7,27 +7,42 @@ app.listen(port,function(){
 })
 
 app.use(express.json());
+const userRouter = express.Router();
+app.use('/user',userRouter);
+
+userRouter
+.route('/')
+.get()
+.post()
+.patch()
+.delete();
 
 let user = {};
 
 //get request
 //client <- server
-app.get("/user",(req,res)=>{
-    res.json(user);
-})
 
+//crud - create read update delete
+//read
 app.get("/",(req,res)=>{
     res.send("Home Page");
 })
 
+app.get("/user",(req,res)=>{
+    res.json(user);
+})
+
+
 //post request
 //client -> server
+//create
 app.post("/user",(req,res)=>{
     user=req.body;
     // console.log(req.body);
     res.send("data has been added successfully");
 })
 
+//update
 app.patch("/user",(req,res)=>{
     let obj = req.body;
     for(let key in obj){
@@ -36,7 +51,15 @@ app.patch("/user",(req,res)=>{
     res.json(user);
 })
 
+//delete
 app.delete("/user",(req,res)=>{
     user={};
     res.json(user);
+})
+
+//param route
+app.get('/user/:id',(req,res)=>{
+    console.log(req.params);
+    // res.send(req.params.id);
+    res.json(req.params.id);
 })
