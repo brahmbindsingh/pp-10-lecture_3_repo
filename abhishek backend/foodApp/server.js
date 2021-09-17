386d654b7d13d7045a08c07d41534a00669788bf
Brahm Bind Singh <singhbrahmbind@gmail.com>
2021-09-17 18:01:48 +0530

    day2

--- abhishek backend/foodApp/server.js
+++ abhishek backend/foodApp/server.js
@@ -1,4 +1,5 @@
 const express=require('express');
+
 const app=express();
 // const router=express.Router();
 app.listen('5000',function(){
@@ -6,9 +7,11 @@ app.listen('5000',function(){
 });
 
 app.use(express.json());
-
+app.use(express.static('public'));
 const userRouter=express.Router();
+const authRouter=express.Router();
 app.use('/user',userRouter);
+app.use('/auth',authRouter);
 //mounting in express
 userRouter
 .route('/')
@@ -16,26 +19,41 @@ userRouter
 .post(createUser)
 .patch(updateUser)
 .delete(deleteUser);
-
 userRouter
 .route('/:id')
 .get(getUserById);
 
+authRouter
+.route('/signup')
+.post(signupUser);
 
-let user={};
+function signupUser(req,res){
+    // let userDetails=req.body;
+    // let name=userDetails.name;
+    // let email=userDetails.email;
+    // let password=userDetails.password;
+
+    let{email,name,password}=req.body;
+    user.push({email,name,password});
+    console.log('user',req.body);
+    res.json({
+        message:'user signedUp',
+        user:req.body
+    });
+}
+
+
+let user=[];
 // client <- server
 //crud- create read update delete
 //read
 app.get('/',(req,res)=>{
     res.send('Home Page');
 });
-
 // app.get('/user',getUser);
-
 function getUser(req,res){
     res.json(user);
 }
-
 //post request
 // client-> server 
 //create
@@ -63,7 +81,6 @@ function deleteUser(req,res){
 }
 //param route
 // app.get('/user/:id',getUserById);
-
 function getUserById(req,res){
     console.log(req.params);
     res.json(req.params.id);
