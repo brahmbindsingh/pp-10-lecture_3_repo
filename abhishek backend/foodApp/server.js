5c45b55773c8ca7787fc8aa9740111aa39aabd19
Brahm Bind Singh <singhbrahmbind@gmail.com>
2021-09-17 14:53:56 +0530

    day2

--- abhishek backend/foodApp/server.js
+++ abhishek backend/foodApp/server.js
@@ -1,65 +1,70 @@
-const express = require('express');
-const app = express();
-let port = '5000';
-
-app.listen(port,function(){
-    console.log(`start listening at ${port}`);
-})
+const express=require('express');
+const app=express();
+// const router=express.Router();
+app.listen('5000',function(){
+    console.log('server listening on port 5000');
+});
 
 app.use(express.json());
-const userRouter = express.Router();
-app.use('/user',userRouter);
 
+const userRouter=express.Router();
+app.use('/user',userRouter);
+//mounting in express
 userRouter
 .route('/')
-.get()
-.post()
-.patch()
-.delete();
+.get(getUser)
+.post(createUser)
+.patch(updateUser)
+.delete(deleteUser);
 
-let user = {};
+userRouter
+.route('/:id')
+.get(getUserById);
 
-//get request
-//client <- server
 
-//crud - create read update delete
+let user={};
+// client <- server
+//crud- create read update delete
 //read
-app.get("/",(req,res)=>{
-    res.send("Home Page");
-})
+app.get('/',(req,res)=>{
+    res.send('Home Page');
+});
 
-app.get("/user",(req,res)=>{
+// app.get('/user',getUser);
+
+function getUser(req,res){
     res.json(user);
-})
-
+}
 
 //post request
-//client -> server
+// client-> server 
 //create
-app.post("/user",(req,res)=>{
+// app.post('/user',createUser);
+function createUser(req,res){
     user=req.body;
     // console.log(req.body);
-    res.send("data has been added successfully");
-})
-
+    res.send('data has been added succesfully');
+}
 //update
-app.patch("/user",(req,res)=>{
-    let obj = req.body;
+// app.patch('/user',updateUser);
+function updateUser (req,res){
+    let obj=req.body;
     for(let key in obj){
         user[key]=obj[key];
     }
     res.json(user);
-})
-
-//delete
-app.delete("/user",(req,res)=>{
+};
+//delete 
+// app.delete('/user',deleteUser);
+function deleteUser(req,res){
     user={};
     res.json(user);
-})
-
+    // res.send('ussr has been deleted');
+}
 //param route
-app.get('/user/:id',(req,res)=>{
+// app.get('/user/:id',getUserById);
+
+function getUserById(req,res){
     console.log(req.params);
-    // res.send(req.params.id);
     res.json(req.params.id);
-})
+}
